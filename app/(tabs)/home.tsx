97b3e22dc0c31932572c04d06ea67c7d97bd62a2
Grace Allas <allas2@wisc.edu>
2025-03-04 17:28:04 -0600

    titles for home page

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform, Text } from 'react-native';
+import { StyleSheet, Image, Platform, Text, View } from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -6,19 +6,57 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
+import { Fontisto } from '@expo/vector-icons';
 
 export default function TabTwoScreen() {
   return (
-    <ParallaxScrollView>
-      <Text>
-        Hi
+    //<ParallaxScrollView>
+    <View>
+    
+      <Text style = {styles.title}>
+        HARMONIZE
       </Text>
-    </ParallaxScrollView>
+      
+      {/* insert profile photo in top right corner */}
+
+      <Text style = {styles.subtitle}>
+        Playlists
+      </Text>
+
+      <Text style = {styles.view}>
+        View all
+      </Text>
+    </View>
+    //</ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   overall: {
     alignItems: 'center',
+  },
+
+  title: {
+    marginVertical: 80,
+    marginLeft: 20,
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+  subtitle: {
+    marginLeft: 20,
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+  view: {
+    marginVertical: -20,
+    fontSize: 15,
+    marginRight: 20,
+    fontWeight: 'medium',
+    color: 'white',
+    textAlign: 'right',
   },
 });
