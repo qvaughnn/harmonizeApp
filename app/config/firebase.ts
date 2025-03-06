91877a7e1449148dd7b5656768c5c582c8ec60c9
JustinBildner <justinbildner@gmail.com>
2025-03-06 11:06:44 -0600

    Configured firebase for ios, android, and web

---
+++ app/config/firebase.ts
@@ -0,0 +1,23 @@
+// Import necessary functions from Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+
+// Firebase configuration (For Web App, IOS and Android in respective files)
+const firebaseConfig = {
+  apiKey: "AIzaSyDJTEYjC494IJcW1QybCTF0iHRO30eowQA",
+  authDomain: "harmonize-37b1a.firebaseapp.com",
+  databaseURL: "https://harmonize-37b1a-default-rtdb.firebaseio.com",
+  projectId: "harmonize-37b1a",
+  storageBucket: "harmonize-37b1a.firebasestorage.app",
+  messagingSenderId: "294773082167",
+  appId: "1:294773082167:web:8d02532ac3848231d2d7b2",
+};
+
+// Initialize Firebase (and database and auth)
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth(app);
+
+// Export the Firebase app instance for use in other parts of the app
+export default app;
