c58bb7abaea5831f56dbc71edc8d3046b98d659b
JustinBildner <justinbildner@gmail.com>
2025-04-20 18:25:45 -0500

    Changed playlist page to use database and added functionality to add songs to a playlist

--- types/song.ts
+++ types/song.ts
@@ -1,9 +1,11 @@
 export type Song = {
   name: string;
   artist: string;
+  album: string;
   duration_ms: number;
   cover_art: string;
   spotify_id: string;
   spotify_uri: string;
   apple_id?: string;
+
 };
