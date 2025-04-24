1893293bf65959c174364390cb594e43ba928dbd
Grace Allas <allas2@wisc.edu>
2025-04-23 23:02:08 -0500

    fixed overlap of last playlist and nav bar

--- app/(tabs)/allPlaylists.tsx
+++ app/(tabs)/allPlaylists.tsx
@@ -169,6 +169,7 @@ const AllPlaylists = () => {
               )}
             />
           )}
+          contentContainerStyle={{ paddingBottom: 100 }}
         />
       </GestureHandlerRootView>
 
